33996a2890d8d088777bda3a08eed2e2dbabab49
mateusz.k <mateusz.k@adchitects.co>
2022-09-18 15:51:14 +0200

    feat(packages/prettier): ✨  Setup the index file

--- packages/prettier/source/index.ts
+++ packages/prettier/source/index.ts
@@ -1,3 +1,15 @@
-export function hello() {
-    console.log("Hello from @adchitects/prettier-config!");
-}
+import { createMergedConfig } from "@workspace/shared/configuration";
+// import { hasModule } from "@workspace/shared/module";
+
+import prettier from "./prettier.js";
+
+const mergedConfig = createMergedConfig([
+	// Base
+	prettier,
+
+	// Plugins
+    // ...
+]);
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = mergedConfig;
