90bad9756c0ce78cbfee6bd02fd2208614b475c7
mateusz.k <mateusz.k@adchitects.co>
2022-09-29 16:28:01 +0200

    refactor(packages/eslint): ♻️  Implement main file (index)

--- packages/eslint/source/index.ts
+++ packages/eslint/source/index.ts
@@ -1,3 +1,13 @@
-export function hello() {
-    console.log("Hello from @adchitects/eslint-config!");
-}
+// This is a workaround for https://github.com/eslint/eslint/issues/3458
+import "@rushstack/eslint-patch/modern-module-resolution";
+
+import eslint from "./eslint.js";
+
+const mergedConfig = createMergedConfig([
+	// Base
+	eslint,
+
+]);
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = mergedConfig;
