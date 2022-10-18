8f46bc38e40a603444eee1a2388e154c4e9f41cf
mateusz.k <mateusz.k@adchitects.co>
2022-10-18 19:40:09 +0200

    chore(ESLint): 🔧  Add config at the root

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+const config = {
+	extends: "@adchitects/eslint-config",
+
+	env: {
+		// Specify the environment(s)
+		// https://eslint.org/docs/user-guide/configuring/language-options#specifying-environments
+		browser: true,
+		node: true,
+	},
+};
+
+module.exports = config;
