8474a2d8af00380d0f0c16e12e411445db959b20
mateusz.k <mateusz.k@adchitects.co>
2022-11-07 15:59:27 +0100

    build(packages/browserslist): 🏗  Change the build structure

---
+++ packages/browserslist/source/browserslist.ts
@@ -0,0 +1,10 @@
+export const config = [
+	// https://browsersl.ist
+	"defaults and supports es6-module",
+	/**
+	 * NOTE:
+	 * Meaning LTS version.
+	 * Any version above LTS is not considered as "major release", AFAIK.
+	 */
+	"last 1 node major versions",
+];
