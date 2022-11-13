c329f4bbd337b47673fac6fc2abb0592f81c0a00
mateusz.k <mateusz.k@adchitects.co>
2022-11-13 20:46:01 +0100

    build(packages/browserslist): 🔧  Tweak `tsup` config

--- packages/browserslist/tsup.config.ts
+++ packages/browserslist/tsup.config.ts
@@ -4,6 +4,8 @@ import { getNodeCJSOptions } from "@adchitects/tsup-config";
 
 export default defineConfig((options) => ({
 	...getNodeCJSOptions(options),
+	bundle: true,
+	dts: false,
 	entry: ["source/index.ts"],
 	noExternal: ["@workspace/shared"],
 }));
