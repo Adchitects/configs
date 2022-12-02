ada684a2671e1b743353117eb1d7fbb579971aa1
mateusz.k <mateusz.k@adchitects.co>
2022-12-02 12:33:20 +0100

    fix(Stylelint): 🐛  Temporarily fix build issues

--- packages/stylelint/tsup.config.ts
+++ packages/stylelint/tsup.config.ts
@@ -4,6 +4,8 @@ import { getNodeCJSOptions } from "@adchitects/tsup-config";
 
 export default defineConfig((options) => ({
 	...getNodeCJSOptions(options),
+	bundle: true,
+	dts: false,
 	entry: ["source/**/*.ts"],
 	noExternal: ["@workspace/shared"],
 }));
